e13092dbdaea20e8ee84c93f77246fcbca66cb97
river-spring <rivermspring@gmail.com>
2020-09-01 12:43:08 +0900

    [fix]マイページの友達申請レイアウト

--- app/assets/javascripts/users/friends.js
+++ app/assets/javascripts/users/friends.js
@@ -1,9 +1,9 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-// フレンドページタブメニューに関するCSS
 
 $(document).on('turbolinks:load', function() {
+	// フレンドページタブメニューに関するCSS
 	$('#friend_tab_contents .friend__tab[id != "frinds_list"]').hide();
 
 	$('#friend__tab_menu a').on('click', function(event) {
@@ -14,4 +14,11 @@ $(document).on('turbolinks:load', function() {
 		$(`#${$(this).data('target')}`).show();
 		event.preventDefault();
 	});
+	$(function(){
+		$('.friend_btn').hover(function(){
+			$('this').text('取消');
+			$('this').css({'background-color': '#d9534f'});
+		});
+	});
 });
+
