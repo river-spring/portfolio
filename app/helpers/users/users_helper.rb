94bbf2a5e2ffa2db8e8829fbed3bd045d8f7dde8
river-spring <rivermspring@gmail.com>
2020-09-06 15:14:27 +0900

    [add]ユーザーログイン画面のエラーメッセージ

--- app/helpers/users/users_helper.rb
+++ app/helpers/users/users_helper.rb
@@ -15,6 +15,25 @@ module Users::UsersHelper
   end
 
   def devise_sign_in_error_message
-
+    # ログインページ初回読み込み時、ユーザーIDがnilのため、メッセージを表示させない。
+    return "" if resource.user_id == nil
+    # ログインできずに、同じページへリダイレクトされた時のメッセージ
+    if params["user"]["user_id"] == "" && params["user"]["password"] == ""
+      msg = "ユーザーIDとパスワードを入力してください。"
+    elsif params["user"]["user_id"] == ""
+      msg = "ユーザーIDを入力してください。"
+    elsif params["user"]["password"] == ""
+      msg = "パスワードを入力してください。"
+    else
+      msg = "ユーザーIDもしくはパスワードが間違っています。"
+    end
+    html = ""
+    # エラーメッセージ用のHTMLを生成
+    html += <<-EOF
+      <div class="error_field alert alert-danger" role="alert">
+        <p class="error_msg">#{msg}</p>
+      </div>
+    EOF
+    html.html_safe
   end
 end
